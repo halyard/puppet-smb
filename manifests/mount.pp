ca925f5fec180fde997c999ecce14e55918386e7
Les Aker <me@lesaker.org>
2022-05-10 09:47:37 -0400

    update options handling

--- manifests/mount.pp
+++ manifests/mount.pp
@@ -1,23 +1,23 @@
 # @summary Create an SMB mountpoint
 #
 # @param share sets the upstream path for the share
+# @param options sets the connection options for the share
 # @param path (namevar) sets the local path for the mount
 define smb::mount (
   String $share,
+  String $options = 'username=NULL,password=NULL',
   String $path = $title,
 ) {
   file { $path:
     ensure => directory,
   }
 
-  $credentials = configvault_read("smb/${path}", false, 'username=NULL,password=NULL')
-
   mount { $path:
     ensure  => mounted,
     device  => $share,
     atboot  => true,
     fstype  => 'cifs',
-    options => $credentials,
+    options => $options,
     dump    => '0',
     pass    => '2',
     require => Package['cifs-utils'],
